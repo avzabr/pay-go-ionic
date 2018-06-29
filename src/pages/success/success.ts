e747481ac5f28cd5e99e628ab094607c59d5f211
Artyom Pranovich <Artsiom.Pranovich@apps.ask.com>
2018-06-29 16:43:07 +0300

    Fix images

--- src/pages/success/success.ts
+++ src/pages/success/success.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home';
-import { Waiter } from '../waiter/waiter';
 
 @Component({
   selector: 'success-payment-page',
